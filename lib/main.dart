f8dd75c4ded74438e12cda58257a7fbd58db1c8a
aryawijayak <aryawijayak2004@gmail.com>
2023-11-08 18:01:25 +0700

    code tugas 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shopping_art/menu.dart';
+import 'package:shopping_art/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Shopping Art',
       theme: ThemeData(
         // This is the theme of your application.
         //
